cf9e5cd20e8a4afc9524c35f04e5128f1216ac70
theTrine333 <trinekaka@gmail.com>
2025-08-05 00:18:34 +0300

    updated application file

--- android/app/src/main/java/com/tesla254/Textly/MainApplication.kt
+++ android/app/src/main/java/com/tesla254/Textly/MainApplication.kt
@@ -2,7 +2,7 @@ package com.tesla254.Textly
 
 import android.app.Application
 import android.content.IntentFilter
-import android.telephony.TelephonyManager
+import android.provider.Telephony
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
@@ -10,12 +10,8 @@ import com.facebook.react.ReactPackage
 import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-import android.content.Intent
-import android.os.Build
-import android.provider.Telephony
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.ReactInstanceManager
-import com.facebook.react.bridge.ReactContext
 import com.tesla254.Textly.receivers.SmsReceiver
 
 class MainApplication : Application(), ReactApplication {
@@ -27,18 +23,23 @@ class MainApplication : Application(), ReactApplication {
     SoLoader.init(this, false)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
 
-    // Register the SMS receiver after the JS context is ready
-    if (mReactNativeHost.reactInstanceManager.hasStartedCreatingInitialContext()) {
-      registerSmsReceiver(mReactNativeHost.reactInstanceManager.currentReactContext as? ReactApplicationContext)
+    val instanceManager = mReactNativeHost.reactInstanceManager
+    if (instanceManager.hasStartedCreatingInitialContext()) {
+      val context = instanceManager.currentReactContext
+      if (context is ReactApplicationContext) {
+        registerSmsReceiver(context)
+      }
     } else {
-      mReactNativeHost.reactInstanceManager.addReactInstanceEventListener {
-        registerSmsReceiver(it as? ReactApplicationContext)
+      instanceManager.addReactInstanceEventListener { context ->
+        if (context is ReactApplicationContext) {
+          registerSmsReceiver(context)
+        }
       }
     }
   }
 
-  private fun registerSmsReceiver(context: ReactApplicationContext?) {
-    if (context != null && smsReceiver == null) {
+  private fun registerSmsReceiver(context: ReactApplicationContext) {
+    if (smsReceiver == null) {
       try {
         smsReceiver = SmsReceiver(context)
         val filter = IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
@@ -54,7 +55,8 @@ class MainApplication : Application(), ReactApplication {
   }
 
   init {
-    mReactNativeHost = ReactNativeHostWrapper(this,
+    mReactNativeHost = ReactNativeHostWrapper(
+      this,
       object : ReactNativeHost(this) {
         override fun getUseDeveloperSupport() = BuildConfig.DEBUG
 
@@ -62,9 +64,7 @@ class MainApplication : Application(), ReactApplication {
           return PackageList(this).packages
         }
 
-        override fun getJSMainModuleName(): String {
-          return "index"
-        }
+        override fun getJSMainModuleName(): String = "index"
       }
     )
   }
