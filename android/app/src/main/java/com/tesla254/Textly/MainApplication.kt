387600acdeeb4d6bb190fbf7a3cfd0a77642cdd3
theTrine333 <trinekaka@gmail.com>
2025-08-04 23:44:25 +0300

    updated application file

--- android/app/src/main/java/com/tesla254/Textly/MainApplication.kt
+++ android/app/src/main/java/com/tesla254/Textly/MainApplication.kt
@@ -4,39 +4,38 @@ import android.app.Application
 import android.content.IntentFilter
 import android.content.res.Configuration
 import android.provider.Telephony
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
+  private var smsReceiver: SmsReceiver? = null
+
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-        this,
-        object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages.toMutableList()
-            // Add our custom SMS package
-            packages.add(SmsPackage())
-            return packages
-          }
-
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    this,
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> {
+        val packages = PackageList(this).packages.toMutableList()
+        // Add custom SMS package
+        packages.add(SmsPackage())
+        return packages
       }
+
+      override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
   )
 
   override val reactHost: ReactHost
@@ -46,22 +45,30 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
-    
-    // Register SMS receiver
-    registerSmsReceiver()
+
+    // Wait for React context to be initialized
+    reactNativeHost.reactInstanceManager.addReactInstanceEventListener { context ->
+      registerSmsReceiver(context)
+    }
+
+    // If already initialized (during fast reload), register immediately
+    if (reactNativeHost.reactInstanceManager.hasStartedCreatingInitialContext()) {
+      registerSmsReceiver(reactNativeHost.reactInstanceManager.currentReactContext)
+    }
   }
 
-  private fun registerSmsReceiver() {
-    try {
-      val smsReceiver = SmsReceiver(null) // Will be set when React context is available
-      val filter = IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
-      registerReceiver(smsReceiver, filter)
-    } catch (e: Exception) {
-      e.printStackTrace()
+  private fun registerSmsReceiver(context: ReactApplicationContext?) {
+    if (context != null && smsReceiver == null) {
+      try {
+        smsReceiver = SmsReceiver(context)
+        val filter = IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
+        registerReceiver(smsReceiver, filter)
+      } catch (e: Exception) {
+        e.printStackTrace()
+      }
     }
   }
 
